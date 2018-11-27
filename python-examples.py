3dd5084c59832bce0963cb34b50df0f694e2f191
pumpkinpai <pumpkinson@gmail.com>
2018-11-27 18:29:14 -0500

    python

---
+++ python-examples.py
@@ -0,0 +1,30 @@
+
+* Collection of example python code for simple things done every damn time
+
+* Don't run it.
+
+* It's python3
+
+
+
+**  START OF IT ALL **
+#/usr/bin/python3
+
+if __name__ == "__main__":
+    exit()
+
+
+
+** STRINGS **
+    - substrings
+        exStr = 'abcdef'
+        exStr[0:3]      >> 'abc'
+        exStr[:2]       >> 'ab'
+        exStr[:-3]      >> 'abc'
+        exStr[3:-1]     >> 'de'
+        exStr[-3:]      >> 'def'
+
+    - lower/capital
+        exStr.lower()
+        exStr.upper()
+
